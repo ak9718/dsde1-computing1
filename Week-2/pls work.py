d02727c7840f4dd2b00a7f4bed4fd6a91c975901
Amber King <ak9718@ic.ac.uk>
2019-10-21 12:45:19 +0100

    Adding file

---
+++ Week-2/pls work.py
@@ -0,0 +1,13 @@
+# This program is a test :)
+
+print ('I see you are in the library.')
+print ('How many hours have you been here?') # asking how long they have been here
+longTime = input()
+print ('Oh  my goodness ' + longTime + ' hours is a long time.')
+
+print ('How much longer are you going to stay?')
+notLong = input()
+print ('Wow, you will have done ' + str(int(longTime) + int(notLong)) + ' hours by then!')
+print ('GOOD FUCKING JOB, BABE' + str((int(longTime) + int(notLong)) * '!'))
+
+
