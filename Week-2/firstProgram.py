ed466e239a6ed62a716696bd8405b476ed07cd8e
Amber King <ak9718@ic.ac.uk>
2019-10-25 13:45:53 +0100

    Adding file

---
+++ Week-2/firstProgram.py
@@ -0,0 +1,17 @@
+message = 'If this has printed, you have run the code successfully.'
+print(message)
+
+message2 = 'How many pints do you want to drink'
+print(message2)
+
+theirAnswer = input()
+
+message3 = 'Wow '
+message4 = ' is quite a few lol'
+
+print(message3 + str(theirAnswer) + message4)
+
+
+
+
+
